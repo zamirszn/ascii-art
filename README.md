c9ce84688cb4506dfa4bd2431bcad94439056562
Mubarak Lawal <mubaraklawal52@gmail.com>
2026-03-27 10:21:02 +0100

    impl works with or without banner cli

--- README.md
+++ README.md
@@ -3,13 +3,13 @@
 
 Ascii Art implementation in golang
 
-Test with e.g 
+Test without banner (default is ) e.g 
 go run . "HeLlO" | cat -e
 
 Test with banner using:
 
 go run main.go "Hello\n" standard | cat -e
 
-TODO: make it work without adding a banner
+
 
 
--- main.go
+++ main.go
@@ -8,16 +8,24 @@ import (
 )
 
 func main() {
-	// go run . "Hello" => os.Args = ["./main", "Hello"]
-	if len(os.Args) != 3 {
+	argCount := len(os.Args)
+
+	// Check if we have at least the input string
+	if argCount < 2 || argCount > 3 {
 		fmt.Println("Usage: go run . [STRING] [banners]")
 		os.Exit(1)
 	}
 
 	input := os.Args[1]
-	banners := os.Args[2]
 
 	// Default banner is "standard"
+	banners := "standard"
+
+	// If the user provided a second argument, override the default
+	if argCount == 3 {
+		banners = os.Args[2]
+	}
+
 	result, err := ascii.Render(input, banners)
 	if err != nil {
 		fmt.Println("Error:", err)
